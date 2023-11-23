bde7552174c8b850b6c1f76a8eb09be1cb1e07c2
Jhorranis <jhorranis@gmail.com>
2023-11-23 00:11:08 -0300

    feat(coffeeCard): add a footer with price and buttons

--- src/components/CoffeeCard/index.tsx
+++ src/components/CoffeeCard/index.tsx
@@ -1,14 +1,32 @@
-import { CoffeeCardContainer, Tags, Tag } from './styles'
-
 import { Coffee } from '../../utils/coffeeData'
+import { Minus, Plus, ShoppingCartSimple } from '@phosphor-icons/react'
+
+import {
+  CoffeeCardContainer,
+  Tags,
+  Tag,
+  Counter,
+  Price,
+  Footer,
+  Actions,
+  CartButton,
+  PriceContainer,
+} from './styles'
 
 interface CoffeCardProps {
   data: Coffee
 }
 
 export function CoffeeCard({
-  data: { name, description, thumbnail, tags },
+  data: { name, description, thumbnail, tags, price },
 }: CoffeCardProps) {
+  const formattedPrice = new Intl.NumberFormat('pt-BR', {
+    style: 'decimal',
+    currency: 'BRL',
+
+    minimumFractionDigits: 2,
+  }).format(price / 100)
+
   return (
     <CoffeeCardContainer>
       <img src={thumbnail} alt="" />
@@ -21,6 +39,33 @@ export function CoffeeCard({
 
       <h3>{name}</h3>
       <span>{description}</span>
+
+      <Footer>
+        <PriceContainer>
+          <Price>
+            R$
+            <span>{formattedPrice}</span>
+          </Price>
+        </PriceContainer>
+
+        <Actions>
+          <Counter>
+            <button title="Diminuir quantidade">
+              <Minus size={14} />
+            </button>
+
+            <span>10</span>
+
+            <button title="Aumentar quantidade">
+              <Plus size={14} />
+            </button>
+          </Counter>
+
+          <CartButton title="Adicionar ao carrinho">
+            <ShoppingCartSimple size={18} weight="fill" />
+          </CartButton>
+        </Actions>
+      </Footer>
     </CoffeeCardContainer>
   )
 }
--- src/components/CoffeeCard/styles.ts
+++ src/components/CoffeeCard/styles.ts
@@ -18,6 +18,8 @@ export const CoffeeCardContainer = styled.div`
 
   img {
     margin-top: -1.25rem;
+    width: 6.25rem;
+    height: 6.25rem;
   }
 
   h3 {
@@ -57,4 +59,104 @@ export const Tag = styled.div`
   padding: 0.25rem 0.5rem;
   border-radius: 100px;
   background: ${({ theme }) => theme['yellow-light']};
+`
+export const Footer = styled.footer`
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  gap: 0.5rem;
+
+  margin-top: 2rem;
+`
+
+export const PriceContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+
+  font-size: 0.875rem;
+  line-height: 1.3;
+  color: ${({ theme }) => theme['base-text']};
+
+  span {
+    font-family: 'Baloo 2', sans-serif;
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: inherit;
+    line-height: inherit;
+
+    margin-left: 0.125rem;
+  }
+`
+
+export const Price = styled.div`
+  display: flex;
+  align-items: baseline;
+`
+
+export const Actions = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`
+
+export const Counter = styled.div`
+  display: flex;
+  gap: 0.25rem;
+
+  border-radius: 6px;
+  background: ${({ theme }) => theme['base-button']};
+
+  padding: 0.5rem;
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    color: ${({ theme }) => theme.purple};
+
+    border: none;
+    background-color: transparent;
+
+    cursor: pointer;
+    transition: color 0.1s;
+
+    &:hover {
+      color: ${({ theme }) => theme['purple-dark']};
+    }
+  }
+
+  span {
+    width: 1.25rem;
+
+    display: flex;
+    align-items: center;
+
+    font-size: 1rem;
+    line-height: 1.3;
+    color: ${({ theme }) => theme['base-title']};
+  }
+`
+
+export const CartButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 0.5rem;
+
+  border: none;
+  border-radius: 6px;
+
+  color: ${({ theme }) => theme['base-card']};
+  background: ${({ theme }) => theme['purple-dark']};
+
+  cursor: pointer;
+
+  transition: background 0.1s;
+
+  &:hover {
+    background: ${({ theme }) => theme.purple};
+  }
 `
